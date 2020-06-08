3acf60bf0a8cf36c0530c6c9fec9094316223cf6
rasheed <rachid.aguo@gmail.com>
2020-06-08 20:37:43 +0100

    first commit

---
+++ src/js/custom.js
@@ -0,0 +1,162 @@
+/* --------------------------------------------------------------------
+-----------------------------------------------------------------------
+   Website Name : Houslink
+   Author : Houslink
+   Version : 1.0.0
+   Created : June 2020
+   File Description : Main JS file
+-----------------------------------------------------------------------
+---------------------------------------------------------------------*/
+
+$(document).ready(function(){
+
+    "use strict";
+
+
+    // Responsive Navigation Bar
+    function mobileMenu() {
+        if ($(window).width() < 1200) {
+          $(".mainmenu").addClass("mobilemenu");
+        } else if ($(window).width() >= 1200) {
+            $(".mainmenu").removeClass("mobilemenu");
+        }
+    }
+    mobileMenu();
+    $(window).resize(function() {
+        mobileMenu();
+    });
+
+    function menuToggle() {
+        $(".mainmenu").find(".mainmenu-toggler").on("click", function () {
+            if($(this).hasClass("open")) {
+                $(this).removeClass("open");
+            } else {
+                $(this).addClass("open");
+            }
+            var menuCollapse = $(this).next('.mainmenu-collapse');
+            if (menuCollapse.hasClass("open")) {
+                menuCollapse.slideUp().removeClass("open");
+            } else {
+                menuCollapse.slideDown().addClass("open");
+            }
+        });
+    }
+    menuToggle();
+
+    $(".mainmenu").find(".has-children > a").click(function() {
+        
+        if ($(".mainmenu").hasClass("mobilemenu")) {
+            var submenuLI = $(this).parent("li:first");
+            if (submenuLI.hasClass("open")) {
+                submenuLI.find("ul:first").slideUp(function(){
+                    submenuLI.removeClass("open");
+                });
+            }
+            else {
+                submenuLI.addClass("open");
+                submenuLI.find("ul:first").slideDown();
+            }
+            
+            return false;
+        }
+        
+    });
+
+    $(".mainmenu").find(".has-children").hover(
+        function() {
+            if (!($(".mainmenu").hasClass("mobilemenu"))) {
+                $(this).find("ul:first").stop(true, true).fadeIn("fast");
+            }
+        }, 
+        function() {
+            if (!($(".mainmenu").hasClass("mobilemenu"))) {
+                $(this).find("ul:first").stop(true, true).delay(100).fadeOut("fast");
+            }
+        }
+    );
+
+    $(window).resize(function() {
+        if ((!$(".mainmenu").hasClass("mobilemenu"))) {
+            $(".mainmenu").find(".has-children > ul").hide();
+            $(".mainmenu").find(".has-children").removeClass("open");
+        }
+    });
+
+    
+    $(".mainmenu .mainmenu-links a").each(function () {
+        if (this.href == window.location.href) {
+            $(this).parent().addClass("active"); 
+            $(this).parent().parent().parent().addClass("active"); 
+            $(this).parent().parent().parent().parent().parent().addClass("active"); 
+        }
+    });
+    if(window.location.pathname == "/"){
+        $(".mainmenu .mainmenu-links > li:first").addClass("active");
+    }
+
+
+    /* Back to Top */ 
+   $(window).on("scroll", function() {
+        if($(this).scrollTop() > 700)
+            $(".back-to-top").fadeIn(); 
+        else
+            $(".back-to-top").fadeOut();
+    });
+    $(".back-to-top").click(function() {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1000);
+    });
+
+
+    /* Preloader */ 
+    $(window).on('load', function() { 
+        $('#preloader').delay(500).fadeOut('slow');
+        $('.spinner').delay(250).fadeOut();
+        $('body').delay(250).css({
+                'overflow': 'visible'
+        });
+    });
+
+
+    // Home Carousel
+    $("#owl-home").owlCarousel({
+        items: 1,
+        loop: true,
+        nav: true,
+        autoplayHoverPause: true,
+        autoplay: 2000,
+        mouseDrag: true,
+        dots:false,
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"]
+    });
+
+    // Services Carousel
+    $('#owl-services').owlCarousel({ 
+        items: 3,
+        loop: true,
+        margin: 40,
+        autoplayHoverPause: true,
+        autoplay: 2500,
+        mouseDrag: true,
+        nav: true,
+        dots: false, 
+        navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+        responsive: {
+            0: {
+                items:2,
+                nav: true
+            },
+            600: {
+                items:3,
+                nav: true
+            },
+            1000: {
+                items: 3,
+                nav: true
+            }
+        }
+    });
+    
+
+});
